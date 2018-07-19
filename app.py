14c75ace196736dda8afe8dcdf6c7ed419a8873a
Yash Mewada <ybmewada@gmail.com>
2018-07-19 16:14:58 -0700

    Added getngos endpoint

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,jsonify
 
 app = Flask(__name__)
 
@@ -7,6 +7,30 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/allngos')
+def get_all_ngos():
+    ngos = [{
+"id": 1,
+"name": "NGO1",
+"Description": "",
+"Score": 76.7,
+"No_of_Donation_Year_per_yr": 12,
+"cost_per_dollar_spent": 0.7,
+"donar_retention_rate": 0.2,
+"optout_rate":4,
+"new_projects":3,
+"extension_rate":4,
+"company_connected":[1,2],
+"donation_money":5000,
+"donation_resources":[{"type":"laptops","amount":456}],
+"category":["No Poverty","Zero Hunger","Quality Education","Gender Equality","Water & Sanitation",
+    "Affordable & Clean energy","Climate Action","Wildlife & Oceanlife"]
+}]
+
+    return jsonify(ngos)
+
 
 if __name__ == '__main__':
     app.run()
+
+
